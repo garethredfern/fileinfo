8fd5166fe0a512b351ee0baf2bd0a525cc673008
Gareth Redfern <gareth.redfern@gmail.com>
2013-08-27 08:55:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-file-info-plugin
-================
+Get File Info
+=============
 
-A very simple Statamic plugin to output file information.
+A simple Statamic plugin to output file information.
+
+## Installing
+
+Drop the `file-info folder` into the Statamic `/_add-ons/` folder.
+
+## Example
+
+~~~
+<ul>
+{{ fileinfo file="/assets/docs/mydocument.pdf" }}
+  <li>Your file extension is: {{ file_ext }}
+  <li>Tour file size is: {{ file_size }}
+  <li>The type of file is: {{ file_type }}
+{{ /fileinfo }}
+</ul>
+~~~
+
+## Parameters
+
+- file
---
+++ fileinfo/pi.fileinfo.php
@@ -0,0 +1,22 @@
+<?php
+class Plugin_fileinfo extends Plugin {
+
+  var $meta = array(
+      'name'       => 'File Info',
+      'version'    => '1',
+      'author'     => 'Gareth Redfern',
+      'author_url' => 'http://garethredfern.com'
+  );
+
+  public function index() {
+    $file     = $this->fetchParam("file");
+
+    $file_info =  array(
+      "file_ext"  => File::getExtension(BASE_PATH . $file),
+      "file_size" => File::getSize(BASE_PATH . $file),
+      "file_type" => File::getType(BASE_PATH . $file),
+    );
+    return $file_info;
+  }
+
+}

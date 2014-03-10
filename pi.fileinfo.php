3caf1bf3934751ffad40ddbfa22497fc2f28762b
Katrin Kerber <katrinkerber@gmail.com>
2014-03-10 16:02:47 +0000

    Added "file_size_kilobytes" as available variable

--- pi.fileinfo.php
+++ pi.fileinfo.php
@@ -10,10 +10,12 @@ class Plugin_fileinfo extends Plugin {
 
   public function index() {
     $file     = $this->fetchParam("file", null, null, null, false);
+    $fileSize = File::getSize(BASE_PATH . $file);
 
     $file_info =  array(
       "file_ext"  => File::getExtension(BASE_PATH . $file),
-      "file_size" => File::getSize(BASE_PATH . $file),
+      "file_size" => $fileSize,
+      "file_size_kilobytes" => number_format($fileSize / 1024)
     );
     return $file_info;
   }
